59ff13cf83973ae7839edb4c8c90a9db01a70852
AnThONy77 <anthonybs.pro@gmail.com>
2022-10-19 16:51:03 +0200

    error display notice

--- index.php
+++ index.php
@@ -15,8 +15,10 @@ use Src\Controllers\ForgotPasswordController;
 
 header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_startup_errors', 1);
+error_reporting(E_WARNING);
+error_reporting(E_ERROR);
+error_reporting(E_NOTICE);
 $request = $_SERVER['REQUEST_URI'];
 $request = explode('?' ,$request, 2);
 $data = null;
