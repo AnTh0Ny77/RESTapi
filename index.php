31d03f56e0b001cdf9a378a23b2cd2e1e7131f63
AnThONy77 <anthonybs.pro@gmail.com>
2022-11-07 10:16:57 +0100

    maj index commercial was delete

--- index.php
+++ index.php
@@ -54,6 +54,9 @@ switch($request){
         echo RefreshController::index($_SERVER['REQUEST_METHOD'],$data);
         break;
 
+    case $config->urls->base.CommercialController::path().$data:
+        echo CommercialController::index($_SERVER['REQUEST_METHOD'],$data);
+
     case $config->urls->base.ConfirmUserController::path().$data:
         echo ConfirmUserController::index($_SERVER['REQUEST_METHOD'],$data);
         break;
