09c20b633097f9fcd9ccd79fc9e2cb2f3ef3ebb9
Anthnoybs <anthonybs.pro@gmail.com>
2022-12-08 16:28:08 +0100

    debug request

--- src/Repository/TicketRepository.php
+++ src/Repository/TicketRepository.php
@@ -314,8 +314,7 @@ Class TicketRepository  extends BaseRepository {
                 ]
             ], 
         ];
-        var_dump('function');
-        die();
+       
         ////////////////////////////////////////////////////////////////////////////////////// LIMIT //////////////////////////////////////////////////////
         $limit_clause = '';
         if (!empty($limit)) {
@@ -418,8 +417,8 @@ Class TicketRepository  extends BaseRepository {
 
     ///////////////////////////////////////////////////////////////////////////////// FINAL ////////////////////////////////////////////////////////////////////////
         $clause = 'SELECT DISTINCT  t.tk__id  FROM ' . $params['self']['name'] . ' as ' . $params['self']['alias'].' '. $left_clause . ' WHERE 1 = 1 ' . $in_clause . ' ' . $where_clause . ' ' .  $orderclause  .'  ' . $limit_clause . '';
-       var_dump($clause);
-       die();
+        var_dump($clause);
+        die();
         $request = $this->Db->Pdo->query($clause);
         return  $request->fetchAll(PDO::FETCH_ASSOC);
     }
