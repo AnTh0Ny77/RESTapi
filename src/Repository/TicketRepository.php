4359b9220ed9a6d37e42b6c0dffe783ffe6fec1a
Anthnoybs <anthonybs.pro@gmail.com>
2022-12-08 16:28:52 +0100

    test why

--- src/Repository/TicketRepository.php
+++ src/Repository/TicketRepository.php
@@ -414,11 +414,11 @@ Class TicketRepository  extends BaseRepository {
         //         $orderclause .= ' '.$key . ' ' . $value . ', ' ;
         //     }
         // }
-
+        var_dump('test');
+        die();
     ///////////////////////////////////////////////////////////////////////////////// FINAL ////////////////////////////////////////////////////////////////////////
         $clause = 'SELECT DISTINCT  t.tk__id  FROM ' . $params['self']['name'] . ' as ' . $params['self']['alias'].' '. $left_clause . ' WHERE 1 = 1 ' . $in_clause . ' ' . $where_clause . ' ' .  $orderclause  .'  ' . $limit_clause . '';
-        var_dump($clause);
-        die();
+        
         $request = $this->Db->Pdo->query($clause);
         return  $request->fetchAll(PDO::FETCH_ASSOC);
     }
