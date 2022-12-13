a2d4bde888c2559f0b0b1d063df505acba746141
AnThONy77 <anthonybs.pro@gmail.com>
2022-12-13 17:37:47 +0100

    maj

--- src/Repository/MaterielRepository.php
+++ src/Repository/MaterielRepository.php
@@ -292,7 +292,8 @@ Class MaterielRepository  extends BaseRepository {
         }
 
         $clause = 'SELECT DISTINCT  *  FROM ' . $params['self']['name'] . ' as ' . $params['self']['alias'].' '. $left_clause . ' WHERE 1 = 1 ' . $in_clause . ' ' . $where_clause . ' ' .  $orderclause  .'  ' . $limit_clause . '';
-     
+        var_dump($clause);
+        die();
         $request = $this->Db->Pdo->query($clause);
         return  $request->fetchAll(PDO::FETCH_ASSOC);
     }
