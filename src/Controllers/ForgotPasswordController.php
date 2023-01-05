53ea37acc3da0ef64aa8257953fe21037c3d737f
Anthnoybs <anthonybs.pro@gmail.com>
2023-01-05 16:02:38 +0100

    debug prod

--- src/Controllers/ForgotPasswordController.php
+++ src/Controllers/ForgotPasswordController.php
@@ -92,9 +92,7 @@ Class ForgotPasswordController  extends  BaseController {
            
         $user = $userRepository->findOneBy(['user__mail' => $_GET['user__mail'] ] , false);
 
-        var_dump('hey');
-        die();
-      
+       
         if(empty($user)) 
             return $responseHandler->handleJsonResponse('Utilisateur inconnu' , 404 , 'Bad Request');
 
