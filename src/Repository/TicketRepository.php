e5c101dbef79f491cb3a02c309e20e4a2e3fbfb9
Anthnoybs <anthonybs.pro@gmail.com>
2023-01-23 16:31:06 +0100

    maj de test

--- src/Repository/TicketRepository.php
+++ src/Repository/TicketRepository.php
@@ -415,6 +415,7 @@ Class TicketRepository  extends BaseRepository {
     ///////////////////////////////////////////////////////////////////////////////// FINAL ////////////////////////////////////////////////////////////////////////
         $clause = 'SELECT DISTINCT  t.tk__id ,   max(y.tkl__dt) AS MTIME   FROM ' . $params['self']['name'] . ' as ' . $params['self']['alias'].' '. $left_clause . ' WHERE 1 = 1 ' . $in_clause . ' ' .   $where_clause . '  GROUP BY t.tk__id
         ORDER BY t.tk__lu ASC , MTIME DESC LIMIT 50 ';
+       
         $request = $this->Db->Pdo->query($clause);
         return  $request->fetchAll(PDO::FETCH_ASSOC);
     }
