6c63fe5776d50433f6b1f02e2492233175f408c8
Anthnoybs <anthonybs.pro@gmail.com>
2023-02-02 10:04:49 +0100

    maj prod

--- src/Services/ResponseHandler.php
+++ src/Services/ResponseHandler.php
@@ -7,6 +7,7 @@ Class ResponseHandler {
 	public function handleJsonResponse($data, int $ResponseCode, string $message){
         $data = json_encode($data);
         header('HTTP/1.0 '.$ResponseCode.' '.$message.'');
+        header("Access-Control-Allow-Origin: *");
         header('Content-Type: application/json; charset=utf-8');
         return $data;
     }
