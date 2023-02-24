f2e6da760ad80e3ac478edd33f8b26db16b54bb0
Anthnoybs <anthonybs.pro@gmail.com>
2023-02-24 17:01:01 +0100

    maj test

--- src/Controllers/TicketLigneController.php
+++ src/Controllers/TicketLigneController.php
@@ -131,7 +131,7 @@ Class TicketLigneController extends BaseController {
         
         if (!empty($dest['user__mail'])){ 
             $dest = $dest['user__nom'] . ' ' . $dest['user__prenom'];
-            $body_mail = $mailer->renderBody($mailer->header(), $mailer->renderBodyTicketEnvoi($body['tkl__tk_id'] , $dest ), $mailer->signature());
+            $body_mail = $mailer->renderBody($mailer->header(), $mailer->renderBodyTicketDest($body['tkl__tk_id'] , $dest ), $mailer->signature());
             $mailer->sendMail($dest['user__mail'] , 'Notification MyRecode' ,  $body_mail );
             // $sender = $user->getUser__nom() . ' ' . $user->getUser__prenom();
             // $body_mail = $mailer->renderBody($mailer->header(), $mailer->renderBodyTicketDest($body['tkl__tk_id'] , $sender ), $mailer->signature());
