544d5322678e739436d6c61113ba2f9393bc6768
Anthnoybs <anthonybs.pro@gmail.com>
2023-06-19 09:05:26 +0200

    debug prod

--- src/Controllers/MailCmdController.php
+++ src/Controllers/MailCmdController.php
@@ -98,12 +98,12 @@ class MailCmdController extends BaseController
         if ($auth != null)
             return $auth;
         
-        
+            var_dump('hey');
+            die();
         $id_user = UserController::returnId__user($security)['uid'];
         $user = $userRepository->findOneBy(['user__id' => $id_user] , true);
 
-        var_dump('hey');
-        die();
+       
         $body = json_decode(file_get_contents('php://input'), true);
         
      
