ef2e743faf9c1765344dd9a83f6156b0962b534d
Anthnoybs <anthonybs.pro@gmail.com>
2023-06-22 11:03:59 +0200

    maj client

--- src/Repository/LienUserClientRepository.php
+++ src/Repository/LienUserClientRepository.php
@@ -14,9 +14,7 @@ Class LienUserClientRepository  extends BaseRepository {
 
     public function getUserClients($user__id){
         $clientRepository = new ClientRepository('client' , $this->Db , Client::class );
-       
         $clients = $this->findBy(['luc__user__id' => $user__id ], 50 , [ 'luc__order' => 'ASC'] );
-       
         $responses = [];
         foreach ($clients as $key => $value) {
             $value = $clientRepository->findOneBy(['cli__id' => $value['luc__cli__id']] ,true);
