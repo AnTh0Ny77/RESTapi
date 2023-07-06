cea455bae0c9612aa7cc4f44b588a5766094d183
Anthnoybs <anthonybs.pro@gmail.com>
2023-07-06 11:28:17 +0200

    finally not

--- src/Controllers/UserSitesSossukeController.php
+++ src/Controllers/UserSitesSossukeController.php
@@ -145,7 +145,7 @@ Class UserSitesSossukeController extends BaseController {
             );
         }
 
-        if(!empty($body['update_parc[]'])) {
+        if(!empty($body['update_parc'])) {
             $user = $userRepository->findOneBy(['user__id' => $body['user__id'] ], true);
             $clients = $lienUserClientRepository->getUserClients($user->getUser__id());
             foreach ($body['update_parc'] as $key => $value){
