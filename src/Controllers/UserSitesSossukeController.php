712eead38177a852737659f1375c5b33828f64c9
Anthnoybs <anthonybs.pro@gmail.com>
2023-08-25 16:14:31 +0200

    debug prod

--- src/Controllers/UserSitesSossukeController.php
+++ src/Controllers/UserSitesSossukeController.php
@@ -145,11 +145,13 @@ Class UserSitesSossukeController extends BaseController {
             );
         }
 
+        return $responseHandler->handleJsonResponse([
+            "data" => $body, 
+        ], 200, '???');
+
        
         if(!empty($body['update'])) {
-            return $responseHandler->handleJsonResponse([
-                "msg" => 'ICI', 
-            ], 401, 'bad request');
+         
             $user = $userRepository->findOneBy(['user__id' => $body['luc__user__id'] ], true);
        
             $clients = $lienUserClientRepository->getUserClients($user->getUser__id());
