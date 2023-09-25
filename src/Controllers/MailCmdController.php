07a6f160b9117d09d21976fb44a2f2f09aed32f1
Anthnoybs <anthonybs.pro@gmail.com>
2023-09-25 16:16:29 +0200

    debug prod

--- src/Controllers/MailCmdController.php
+++ src/Controllers/MailCmdController.php
@@ -23,6 +23,8 @@ use Src\Repository\ClientRepository;
 use Src\Repository\ShopAVendreRepository;
 use Src\Repository\ShopCmdRepository;
 use Src\Repository\ShopCmdLigneRepository;
+use GuzzleHttp\ClientHtpp;
+use GuzzleHttp\Promise;
 use Src\Entities\ShopCmd;
 use Src\Entities\ShopCmdLigne;
 use Src\Controllers\NotFoundController;
@@ -119,16 +121,26 @@ class MailCmdController extends BaseController
       
         $ligne = $shopCmdLigneRepository->findBy(['scl__scm_id' =>  $body['scm__id'] ] , 100 , ['scl__id' => 'ASC']);
         $def_array = [];
+        $sossuke_array = [];
         foreach ($ligne as $key => $value){
             $array_item = $value;
             $avendre = $shopAvendreRepository->findOneBy(['sav__id' =>  $value['scl__ref_id'] ], false);
             $article = $ShopArticleRepository->findOneBy(['sar__ref_id' => $avendre['sav__ref_id']] , false);
             $array_item['temp'] = $article;
             array_push($def_array , $array_item);
+            $sossuke_line = [ ];
+            $sossuke_line['scl__ref_id'] = $value['scl__ref_id'];
+            $sossuke_line['scl__prix_unit'] = $value['scl__prix_unit'];
+            $sossuke_line['scl__qte'] = $value['scl__qte'];
+            $sossuke_line['scl__gar_mois'] = $value['scl__gar_mois'];
+            $sossuke_line['scl__gar_prix'] = $value['scl__gar_prix'];
+            $sossuke_line['sar__description'] = $article['sar__description'];
+            $sossuke_line['sav__etat'] = $avendre['sav__etat'];
+            $sossuke_line['sav__gar_std'] = $avendre['sav__gar_std'];
+            $sossuke_line['sar__ref_constructeur'] = $article['sar__ref_constructeur'];
+            array_push( $sossuke_array , $sossuke_line);
         }
 
-        $body_mail = $mailer->renderBody($mailer->header(), $mailer->renderBodyCommande($cmd ,$def_array), $mailer->signature()); 
-        $mailer->sendMail( $user->getUser__mail(), 'Confirmation de votre commande MyRecode',  $body_mail);
 
         $results =  $clientRepository->findOneBy(['cli__id' => $cmd['scm__client_id_fact']] , true);
                    
@@ -139,6 +151,35 @@ class MailCmdController extends BaseController
                                 'msg' => 'Le commercial du client n a pas été trouvé'
                             ] , 400 , 'Bad Request');
                         }else{
+
+                            //////////////////////////////////////
+                            //creation de la commande sur sossuke :
+                            $sossuke_commande = [];
+                            $sossuke_commande['scm__user_id'] = $results->getCli__com1();
+                            $sossuke_commande['scm__user_id'] = $cmd['scm__prix_port'];
+                            $sossuke_commande['scm__client_id_livr'] = $cmd['scm__client_id_livr'];
+                            $sossuke_commande['scm__client_id_fact'] = $cmd['scm__client_id_fact'];
+                            $sossuke_commande['ligne'] = $sossuke_array;
+                            $sossuke_commande['secret'] = "heAzqxwcrTTTuyzegva^5646478§§uifzi77..!yegezytaa9143ww98314528";
+                            //envoi à l'api de sossuke/////////////////
+                            $config = json_decode(file_get_contents('config.json'));
+                            $guzzle = new \GuzzleHttp\Client(['base_uri' => $config->guzzle->host]);
+                            try {
+                                $response = $guzzle->post('/SoftRecode/apiCmdTransfert', [ 'body' => $sossuke_commande ]);
+                            } catch (ClientException $exeption) {
+                                $response = $exeption->getResponse();
+                            }
+
+                            var_dump($response);
+                            die();
+                            //mis a jour de l ID de la commande et de l ID des lignes 
+
+                            //ENVOI DES 2 MAILS A JOUR 
+                            //////////////////////////////////////////////
+                            //////////////////////////////////////////////
+                            //////////////////////////////////////////////
+                            $body_mail = $mailer->renderBody($mailer->header(), $mailer->renderBodyCommande($cmd ,$def_array), $mailer->signature()); 
+                            $mailer->sendMail( $user->getUser__mail(), 'Confirmation de votre commande MyRecode',  $body_mail);
                             $mailer->sendMail( $com->getCom__email(), 'Une commande vous à été passée par '.$results->getCli__nom().'',  $body_mail);
                         }
                     } else return $responseHandler->handleJsonResponse('Aucun clients facturé trouvés' , 404 , 'Not Found');
