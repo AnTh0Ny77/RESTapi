2d6afeda3e2d99cec7d834f1ec1595b4bd15d2a1
Anthnoybs <anthonybs.pro@gmail.com>
2023-10-05 12:31:51 +0200

    debug prod

--- src/Controllers/UserSitesController.php
+++ src/Controllers/UserSitesController.php
@@ -202,9 +202,9 @@ Class UserSitesController extends BaseController {
             ], 401, 'bad request');
         }
 
-        if (!empty($body['luc__cata'])) { $cata = $body['luc__cata'];}else{$cata = 2;}
+        if (isset($body['luc__cata'])) { $cata = $body['luc__cata'];}else{$cata = 1;}
 
-        if (!empty($body['luc__parc'])) { $parc = $body['luc__parc']; }else{$parc = 2;}
+        if (isset($body['luc__parc'])) { $parc = $body['luc__parc']; }else{$parc = 1;}
 
         $data = [
             'luc__user__id' => $body['luc__user__id'] , 
